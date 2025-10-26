969e3f00d5c697d4d88b6f6a6dbf29ed598082ed
aaditya-ahire-dev <aadity826385@gmail.com>
2025-10-26 20:26:06 +0530

    fix/homeScreen fresh rating updation

--- app/(users)/page.jsx
+++ app/(users)/page.jsx
@@ -2,6 +2,8 @@ import HomePageClient from '@/app/components/HomePageClient'
 import { getAllBooks } from '@/app/services/Api'
 import React from 'react'
 
+export const dynamic = 'force-dynamic';
+
 async function page() {
     const response = await getAllBooks()
 
