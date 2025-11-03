d5e8cd25fd6a1e08f76e4bd9b81c05b375a45a3b
aaditya-ahire-dev <aadity826385@gmail.com>
2025-11-03 11:57:08 +0530

    fix/taiwind problem

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
+export default {
+  plugins: {
+    '@csstools/postcss-oklab-function': {}, 
+    '@tailwindcss/postcss': {},
+  },
+}
 export default config;
